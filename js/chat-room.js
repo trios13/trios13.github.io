8c93318a5c77e29d184481ba9abc1d80b5eeab25
trios13 <troy.rios@gmail.com>
2020-05-12 04:23:09 -0400

    trying to fix javascript code

--- js/chat-room.js
+++ js/chat-room.js
@@ -1,138 +1,129 @@
-const CHANNEL_ID = "h2L493GkS9NV4gpX";
-const drone = new ScaleDrone(CHANNEL_ID, {
-    data: {
-        name: getRandomName(),
-        color: getRandomColor(),
-    },
+const CLIENT_ID = 'h2L493GkS9NV4gpX';
+const drone = new ScaleDrone(CLIENT_ID, {
+  data: { // Will be sent out as clientData via events
+    name: getRandomName(),
+    color: getRandomColor(),
+  },
 });
 
-//Keeps track of users.
 let members = [];
 
-//Connect to Scaledrone room
 drone.on('open', error => {
-    if(error) {
-        return console.error(error);
+  if (error) {
+    return console.error(error);
+  }
+  console.log('Successfully connected to Scaledrone');
+
+  const room = drone.subscribe('observable-room');
+  room.on('open', error => {
+    if (error) {
+      return console.error(error);
     }
-    console.log('Successfully connected to Scaledrone');
+    console.log('Successfully joined room');
+  });
 
-    const room = drone.subscribe('observable-chat');
-    room.on('open', error => {
-        if (error) {
-            return console.error(error);
-        }
-        console.log('Successfully joined room');
-    });
+  room.on('members', m => {
+    members = m;
+    updateMembersDOM();
+  });
 
-    //list of users in the room
-    room.on('members', m => {
-        members = m;
-        updateMembersDOM(); 
-    });
+  room.on('member_join', member => {
+    members.push(member);
+    updateMembersDOM();
+  });
 
-    //User Joining the Room
-    room.on('member_join', member => {
-        members.push(member);
-        updateMembersDOM(); 
-    });
+  room.on('member_leave', ({id}) => {
+    const index = members.findIndex(member => member.id === id);
+    members.splice(index, 1);
+    updateMembersDOM();
+  });
 
-    //User leaving room
-    room.on('member_leave', ({id}) => {
-        const index = members.findIndex(member => member.id === id);
-        members.splice(index, 1);
-        updateMembersDOM();
-    });
-
-    //Listen for messages sent
-    room.on('data', (text, member) => {
-        if (member) {
-            addMessageToListDOM(text, member); 
-        } 
-        else {
-            //Messge is from server
-        }
-    });
+  room.on('data', (text, member) => {
+    if (member) {
+      addMessageToListDOM(text, member);
+    } else {
+      // Message is from server
+    }
+  });
 });
 
 drone.on('close', event => {
-    console.log('Connection was closed', event);
+  console.log('Connection was closed', event);
 });
 
 drone.on('error', error => {
-    console.error(error);
+  console.error(error);
 });
 
-
 function getRandomName() {
-    const adjs = ["autumn", "hidden", "bitter", "silent"];
-    const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning"]; 
-    return (
-        adjs[Math.floor(Math.random() * adjs.length)] + "_" +
-        nouns[Math.floor(Math.random() * nouns.length)]
-    );
-};
-
-function getRandomColor() {
-    return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  const adjs = ["autumn", "hidden", "bitter", "misty", "silent", "empty", "dry", "dark", "summer", "icy", "delicate", "quiet", "white", "cool", "spring", "winter", "patient", "twilight", "dawn", "crimson", "wispy", "weathered", "blue", "billowing", "broken", "cold", "damp", "falling", "frosty", "green", "long", "late", "lingering", "bold", "little", "morning", "muddy", "old", "red", "rough", "still", "small", "sparkling", "throbbing", "shy", "wandering", "withered", "wild", "black", "young", "holy", "solitary", "fragrant", "aged", "snowy", "proud", "floral", "restless", "divine", "polished", "ancient", "purple", "lively", "nameless"];
+  const nouns = ["waterfall", "river", "breeze", "moon", "rain", "wind", "sea", "morning", "snow", "lake", "sunset", "pine", "shadow", "leaf", "dawn", "glitter", "forest", "hill", "cloud", "meadow", "sun", "glade", "bird", "brook", "butterfly", "bush", "dew", "dust", "field", "fire", "flower", "firefly", "feather", "grass", "haze", "mountain", "night", "pond", "darkness", "snowflake", "silence", "sound", "sky", "shape", "surf", "thunder", "violet", "water", "wildflower", "wave", "water", "resonance", "sun", "wood", "dream", "cherry", "tree", "fog", "frost", "voice", "paper", "frog", "smoke", "star"];
+  return (
+    adjs[Math.floor(Math.random() * adjs.length)] +
+    "_" +
+    nouns[Math.floor(Math.random() * nouns.length)]
+  );
 }
 
-function createName() {
-    // create function to allow user to create their own username
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
 
-};
+//------------- DOM STUFF
 
 const DOM = {
-    memberCount: document.querySelector('.member-count'),
-    memberList: document.querySelector('.member-list'),
-    messages: document.querySelector('.messages'),
-    input: document.querySelector('.message-form_input'),
-    form: document.querySelector('.message-form'),
+  membersCount: document.querySelector('.members-count'),
+  membersList: document.querySelector('.members-list'),
+  messages: document.querySelector('.messages'),
+  input: document.querySelector('.message-form__input'),
+  form: document.querySelector('.message-form'),
 };
 
 DOM.form.addEventListener('submit', sendMessage);
 
 function sendMessage() {
-    const value = DOM.input.value;
-    if (value === '') {
-        return;
-    }
-    DOM.input.value = '';
-    drone.publish({
-        room: 'observable-chat',
-        message: value,
-    });
+  const value = DOM.input.value;
+  if (value === '') {
+    return;
+  }
+  DOM.input.value = '';
+  drone.publish({
+    room: 'observable-room',
+    message: value,
+  });
 }
 
 function createMemberElement(member) {
-    const {name, color} = member.clientData;
-    const elem = document.createElement('div');
-    elem.appendChild(document.createTextNode(name));
-    elem.className = 'member';
-    elem.style.color = color;
-    return elem;
+  const { name, color } = member.clientData;
+  const el = document.createElement('div');
+  el.appendChild(document.createTextNode(name));
+  el.className = 'member';
+  el.style.color = color;
+  return el;
 }
 
 function updateMembersDOM() {
-    DOM.memberCount.innerText = '${members.length} users in room: ';
-    DOM.memberList.innerHTML = '';
-    members.forEach(member => 
-        DOM.memberList.appendChild(createMemberElement(member))
-    );
+  DOM.membersCount.innerText = `${members.length} users in room:`;
+  DOM.membersList.innerHTML = '';
+  members.forEach(member =>
+    DOM.membersList.appendChild(createMemberElement(member))
+  );
 }
 
 function createMessageElement(text, member) {
-    const elem = document.createElement('div');
-    elem.appendChild(createMemberElement(member));
-    elem.appendChild(document.createTextNode(text));
-    elem.className = 'message';
-    return elem;
+  const el = document.createElement('div');
+  el.appendChild(createMemberElement(member));
+  el.appendChild(document.createTextNode(text));
+  el.className = 'message';
+  return el;
 }
 
 function addMessageToListDOM(text, member) {
-    const elem = DOM.messages;
-    const wasTop = elem.scrollTop === elem.scrollHeight - elem.clientHeight;
-    elem.appendChild(createMessageElement(text, memeber));
-    if (wasTop) {
-        elem.scrollTop = elem.scrollHeight - elem.clientHeight;
-    }
+  const el = DOM.messages;
+  const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+  el.appendChild(createMessageElement(text, member));
+  if (wasTop) {
+    el.scrollTop = el.scrollHeight - el.clientHeight;
+  }
 }
+
